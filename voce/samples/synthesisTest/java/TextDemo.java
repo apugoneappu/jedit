432187d578339fbf23ee2f15dcd2dbeab222f022
Apoorve Singhal <sapoorve@outlook.com>
2019-03-27 21:58:07 +0530

    moved all samples to a single folder, created textbox, button, padding, button in TextDemo.java

---
+++ voce/samples/synthesisTest/java/TextDemo.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TextDemo extends JPanel implements ActionListener {
+
+  protected JEditorPane editorPane;
+  protected JButton button;
+  private final static String newline = "\n";
+
+  public TextDemo() {
+    super(new GridBagLayout());
+
+    button = new JButton("Print Text");
+    editorPane = new JEditorPane();
+
+    JScrollPane scrollPane = new JScrollPane(editorPane);
+
+    //Add Components to this panel.
+    GridBagConstraints c = new GridBagConstraints();
+    c.gridwidth = GridBagConstraints.REMAINDER;
+    c.insets = new Insets(5,5,2,5);
+    c.fill = GridBagConstraints.BOTH;
+    c.weightx = 1.0;
+    c.weighty = 1.0;
+    add(scrollPane, c);
+
+    c.gridwidth = GridBagConstraints.REMAINDER;
+    c.insets = new Insets(2,5,5,5);
+    c.fill = GridBagConstraints.BOTH;
+    c.weightx = 1.0;
+    c.weighty = 1.0;
+    add(button, c);
+
+    //add(button, c);
+  }
+
+  public void actionPerformed(ActionEvent evt) {
+    String text = editorPane.getText();
+
+    //text to speech here
+  }
+
+  /**
+   * Create the GUI and show it.  For thread safety,
+   * this method should be invoked from the
+   * event dispatch thread.
+   */
+  private static void createAndShowGUI() {
+    //Create and set up the window.
+    JFrame.setDefaultLookAndFeelDecorated(true);
+
+    JFrame frame = new JFrame("TextDemo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Add contents to the window.
+    frame.getContentPane().add(new TextDemo());
+
+    frame.setIconImage(new ImageIcon("yo.png").getImage());
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    //Schedule a job for the event dispatch thread:
+    //creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        createAndShowGUI();
+      }
+    });
+  }
+}
